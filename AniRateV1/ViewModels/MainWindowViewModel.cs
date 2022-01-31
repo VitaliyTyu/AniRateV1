f1baa3e6fe9592836c3e943fe9e422ea72dafb3a
VitaliyTyu <VitaliyTyu@yandex.ru>
2022-01-31 12:41:24 +0500

    Add project files.

---
+++ AniRateV1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using AniRateV1.Infrostructure.Commands;
+using AniRateV1.Models;
+using AniRateV1.ViewModels.Base;
+
+namespace AniRateV1.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+    }
+}
